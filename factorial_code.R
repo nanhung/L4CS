51ef9d033475f31c4d10c51b3e5ff038506db4cf
Nan-Hung Hsieh <nh@nanhung.me>
2018-04-29 10:51:18 -0500

    Update factorial_code.R

--- factorial_code.R
+++ factorial_code.R
@@ -1,28 +1,19 @@
-Factorial_loop <- function(x){
-  if (x==0){
+Factorial_loop <- function(n){
+  if (n==0){
     y <- 1
   } else {
-    y<-1
-    for (i in 1:x){
-      y <-y*((1:x)[i])
+    y <- 1
+    for (i in 1:n){
+      y <- y * ((1:n)[i])
     }
   }
   print(y)
 }
 
-Factorial_reduce <- function(data){
-  library(purrr)
-  if (data==0){
-  1
-  } else
-  reduce(c(1:data), function(x, y) x*y)
+Factorial_reduce <- function(n){
+  if (n==0) 1 else purrr::reduce(c(1:n), function(x, y) x*y)
 }
-         
-Factorial_func <- function(x) {
-  if (x == 0) {
-    1
-  }
-  else {
-    x * Factorial_func(x-1)
-  }
-}         
+
+Factorial_func <- function(n) {
+  if (n == 0) 1 else n * Factorial_func(n-1)
+} 
