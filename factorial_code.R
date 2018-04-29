00be244e7582b98737d35ac11962b013a3ae8efb
Nan-Hung Hsieh <nh@nanhung.me>
2018-04-29 11:46:29 -0500

    Update factorial_code.R

--- factorial_code.R
+++ factorial_code.R
@@ -1,3 +1,6 @@
+## Part 1: Factorial Function ----
+
+## Factorial_loop: a version that computes the factorial of an integer using looping (such as a for loop) ----
 Factorial_loop <- function(n){
   if (n==0){
     y <- 1
@@ -10,10 +13,34 @@ Factorial_loop <- function(n){
   print(y)
 }
 
+## Factorial_reduce: a version that computes the factorial using the reduce() function in the purrr package ----
 Factorial_reduce <- function(n){
   if (n==0) 1 else purrr::reduce(c(1:n), function(x, y) x*y)
 }
 
+## Factorial_func: a version that uses recursion to compute the factorial ----                                  
 Factorial_func <- function(n) {
   if (n == 0) 1 else n * Factorial_func(n-1)
 } 
+                                 
+## Factorial_mem: a version that uses memoization to compute the factorial ----
+                                 
+mem <- function(){
+  
+  out <- 1
+  
+  Factorial_mem <- function(x){
+    
+    if(x < 0) stop()
+    
+    if (x == 0 | x == 1) return(1)
+    
+    if (length(out) < x) out <<- `length<-`(out, x)
+    
+    out[x] <<- x * factorial(x-1)
+    out[x]
+  }
+  Factorial_mem
+}
+
+Factorial_mem <- mem()
