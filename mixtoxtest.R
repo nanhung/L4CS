deb8366fc9d9097b29432d4c57597948118bd0fb
nanhung <nhsieh@cvm.tamu.edu>
2018-09-18 15:42:48 -0500

    test 1st chemical

--- mixtoxtest.R
+++ mixtoxtest.R
@@ -5,6 +5,8 @@ library(ggplot2)
 library(dplyr)
 library(plyr)
 library(tidyr) #seperate
+library(broom)
+
 
 df <- read.csv("mixtoxtest.csv")
 colnames(df)<-c("chemical", "1_r1","1_r2", "2_r1","2_r2", 
@@ -14,27 +16,50 @@ DF$dosen <- as.numeric(DF$dose)
 DF1 <- DF %>% mutate(dose = 10^(dosen-3))
 colnames(DF1)[4]<-"response"
 
+dfChem <- DF1 %>% group_by(chemical) %>%
+  do(fitw = lm(response~dose, data = .))
+
+dfChemCoef <- tidy(dfChem, fitw)
+
+dfChemdose <- dfChemCoef %>% filter(term %in% "dose")
+
+png(file="dose_response.png",width=5200,height=2800,res=300)
 ggplot(DF1, aes(x = dose, y = response)) +
   geom_point(aes(colour = round))+
   geom_path(aes(colour = round), size = 0.1) + 
   facet_wrap( ~ chemical, ncol = 7) + 
   scale_x_log10() + theme_bw()
+dev.off()
 
-df1 <- filter(DF1, chemical == "ALDRIN")
-fitw <- lm(response~dose, data = df1)
-ttw<-tukeytrendfit(fitw, dose="dose", scaling = c("ari", "ord", "log"))
-s<-summary(asglht(ttw))
-s$test$tstat
-s$test$pvalues[1]
-  
-df2 <- filter(DF1, chemical == "BENZIDINE")
-fitw <- lm(response~dose, data = df2)
-ttw<-tukeytrendfit(fitw, dose="dose", scaling = c("ari", "ord", "log"))
-summary(asglht(ttw))
+png(file="significant_test.png",width=3600,height=2800,res=300)
+ggplot(dfChemdose, aes(x = estimate, y = -log(p.value), label = chemical)) +
+  xlab(expression('slope'))+
+  ylab("-log10 (p-value)") +
+  geom_hline(yintercept=-log(0.05), col = "#800000", linetype = "dotdash")+
+  annotate("text", x = -1.2, y = 3.5, label = "p = 0.05", col = "#800000")+
+  geom_label(size = 2)
+dev.off()
 
-df3 <- filter(DF1, chemical == "LEAD (Nitrate)")
-fitw <- lm(response~dose, data = df3)
-ttw<-tukeytrendfit(fitw, dose="dose", scaling = c("ari", "ord", "log"))
-summary(asglht(ttw))
+#tukeytrendfit
+#df1 <- filter(DF1, chemical == "ALDRIN")
+#fitw <- lm(response~dose, data = df1)
+#ttw<-tukeytrendfit(fitw, dose="dose", scaling = c("ari", "ord", "log"))
+#s<-summary(asglht(ttw))
+#s$test$pvalues
+
+df1 <- DF1 %>% filter(chemical == "ALDRIN") %>% filter(round == "r1")
+df2 <- DF1 %>% filter(chemical == "ALDRIN") %>% filter(round == "r2")
+conc <- df1$dose
+response <- cbind(df1$response, df2$response)
+response <- apply(response, 2, function(x) x/max(x))
+
+tuneFit1 <- tuneFit(conc, rowMeans(response), eq = "Hill")
+tuneFit1
+
+fit1 <- curveFit(conc, rowMeans(response), eq = "Hill", 
+                 param = c(tuneFit1$sta[1], tuneFit1$sta[2]))
+
+figPlot(fit1, xlab=expression(paste("Conc. (", mu,"M)")), ylab="Inhibition (%)")
+mtext(paste0("ALDRIN (Hill);"," r2 = ",round(tuneFit1$sta[3],3)))
 
 
