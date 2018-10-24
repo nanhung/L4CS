4864af91f43067ef9e1d365acecb4ca8e47d3b43
Hsieh <NHsieh@cvm.tamu.edu>
2018-10-24 17:36:28 -0500

    mixture unit

--- mixturetox.R
+++ mixturetox.R
@@ -38,12 +38,61 @@ dev.off()
 DR <- function(i){
   sheets <- readxl::excel_sheets("Mixture_Neuron.xlsx")
   c.df <- readxl::read_xlsx("Mixture_Neuron.xlsx", sheet = sheets[1])
-  pct <- 1/42
-  c.df$pct.MW.POD.H <- c.df$`Molecular weight` * pct
-  c.df$ugl <- c.df$`Molecular weight` * c.df$POD.Highest
-  mix.MW <- sum(c.df$pct.MW.POD.H)
-  mix.ugl <- mean(c.df$ugl)
-  mixuM <- mix.ugl / mix.MW
+  
+  c.df$pct.MW.AC50.L <- c.df$`Molecular weight` * c.df$`Min. AC50`/sum(c.df$`Min. AC50`)
+  c.df$pct.MW.AC50.H <- c.df$`Molecular weight` * c.df$`Max. AC50`/sum(c.df$`Max. AC50`)
+  c.df$pct.MW.Css.L <- c.df$`Molecular weight` * c.df$Css.medExpos_medRTK.plasma.uM/sum(c.df$Css.medExpos_medRTK.plasma.uM)
+  c.df$pct.MW.Css.H <- c.df$`Molecular weight` * c.df$Css.95percExpos_95RTK.plasma.uM/sum(c.df$Css.95percExpos_95RTK.plasma.uM)
+  c.df$pct.MW.RFD.L <- c.df$`Molecular weight` * c.df$RFD.Low/sum(c.df$RFD.Low)
+  c.df$pct.MW.RFD.H <- c.df$`Molecular weight` * c.df$RFD.High/sum(c.df$RFD.High)
+  c.df$pct.MW.POD.L <- c.df$`Molecular weight` * c.df$POD.Lowest/sum(c.df$POD.Lowest)
+  c.df$pct.MW.POD.H <- c.df$`Molecular weight` * c.df$POD.Highest/sum(c.df$POD.Highest)
+  
+  c.df$ugl.AC50.L <- c.df$`Molecular weight` * c.df$`Min. AC50`
+  c.df$ugl.AC50.H <- c.df$`Molecular weight` * c.df$`Max. AC50`
+  c.df$ugl.Css.L <- c.df$`Molecular weight` * c.df$Css.medExpos_medRTK.plasma.uM
+  c.df$ugl.Css.H <- c.df$`Molecular weight` * c.df$Css.95percExpos_95RTK.plasma.uM
+  c.df$ugl.RFD.L <- c.df$`Molecular weight` * c.df$RFD.Low
+  c.df$ugl.RFD.H <- c.df$`Molecular weight` * c.df$RFD.High
+  c.df$ugl.POD.L <- c.df$`Molecular weight` * c.df$POD.Lowest
+  c.df$ugl.POD.H <- c.df$`Molecular weight` * c.df$POD.Highest
+
+  #
+  mix.MW.AC50.L <- sum(c.df$pct.MW.AC50.L)
+  mix.ugl.AC50.L <- mean(c.df$ugl.AC50.L)
+  mixuM.AC50.L <- mix.ugl.AC50.L / mix.MW.AC50.L
+  
+  mix.MW.AC50.H <- sum(c.df$pct.MW.AC50.H)
+  mix.ugl.AC50.H <- mean(c.df$ugl.AC50.H)
+  mixuM.AC50.H <- mix.ugl.AC50.H / mix.MW.AC50.H
+  
+  mix.MW.Css.L <- sum(c.df$pct.MW.Css.L)
+  mix.ugl.Css.L <- mean(c.df$ugl.Css.L)
+  mixuM.Css.L <- mix.ugl.Css.L / mix.MW.Css.L
+  
+  mix.MW.Css.H <- sum(c.df$pct.MW.Css.H)
+  mix.ugl.Css.H <- mean(c.df$ugl.Css.H)
+  mixuM.Css.H <- mix.ugl.Css.H / mix.MW.Css.H
+  
+  mix.MW.RFD.L <- sum(c.df$pct.MW.RFD.L)
+  mix.ugl.RFD.L <- mean(c.df$ugl.RFD.L)
+  mixuM.RFD.L <- mix.ugl.RFD.L / mix.MW.RFD.L
+  
+  mix.MW.RFD.H <- sum(c.df$pct.MW.RFD.H)
+  mix.ugl.RFD.H <- mean(c.df$ugl.RFD.H)
+  mixuM.RFD.H <- mix.ugl.RFD.H / mix.MW.RFD.H
+  
+  mix.MW.POD.L <- sum(c.df$pct.MW.POD.L)
+  mix.ugl.POD.L <- mean(c.df$ugl.POD.L)
+  mixuM.POD.L <- mix.ugl.POD.L / mix.MW.POD.L
+  
+  mix.MW.POD.L <- sum(c.df$pct.MW.POD.L)
+  mix.ugl.POD.L <- mean(c.df$ugl.POD.L)
+  mixuM.POD.L <- mix.ugl.POD.L / mix.MW.POD.L
+  
+  mix.MW.POD.H <- sum(c.df$pct.MW.POD.H)
+  mix.ugl.POD.H <- mean(c.df$ugl.POD.H)
+  mixuM.POD.H <- mix.ugl.POD.H / mix.MW.POD.H
 
   df <- readxl::read_xlsx("Mixture_Neuron.xlsx", sheet = sheets[i])
   df <- as.data.frame(df)
@@ -57,11 +106,11 @@ DR <- function(i){
                   "5_r1","5_r2","5_r3","5_r4","5_r5","5_r6")
   DF <- df %>% reshape::melt() %>% separate(variable, c("dose", "round")) 
   DF$dosen <- as.numeric(DF$dose)
-  DF1 <- DF %>% mutate(dilution = 10^(dosen-5)) %>% mutate(conc = 10^(dosen-5) * mixuM)
+  DF1 <- DF %>% mutate(dilution = 10^(dosen-5)) %>% mutate(conc = 10^(dosen-5) * mixuM.POD.H)
   colnames(DF1)[4]<-"response"
   
-#  ggplot(DF1, aes(x = dilution, y = response)) +
-   ggplot(DF1, aes(x = conc, y = response)) +
+  ggplot(DF1, aes(x = dilution, y = response)) +
+#   ggplot(DF1, aes(x = conc, y = response)) +
     geom_point(aes(colour = round))+
     geom_path(aes(colour = round), size = 0.1) + 
     facet_wrap( ~ chemical, ncol = 4) + theme_bw() +
